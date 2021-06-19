1803b28be119bc70f12b657eab80a1f37229c5c5
Shafqat Ali <shafqatalirafaqat@gmail.com>
2021-06-19 12:12:07 +0500

    new chagnes

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,15 @@
 <template>
   <v-app>
-    <TheHeader />
     <v-main>
       <router-view />
     </v-main>
     <notifications position="top right" class="mt-5" :closeOnClick="true" />
-    <TheFooter/>
   </v-app>
 </template>
 
 <script>
-import TheHeader from "./components/TheHeader";
-import TheFooter from "./components/TheFooter";
 export default {
   name: "App",
- components: {
-    TheHeader,
-    TheFooter
-  },
   data: () => ({
     //
   }),
